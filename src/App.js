4de3d82f8b798d15752259f057cbf2016822c587
Guillaume DJIODJIO <guillaumedjio@gmail.com>
2023-01-01 00:35:41 +0100

    implementing maps and cards

--- src/App.js
+++ src/App.js
@@ -1,21 +1,91 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { CssBaseline, Grid } from "@material-ui/core";
 
 import Header from './components/Header/Header'
 import List from './components/List/List'
 import Map from './components/Map/Map'
 
+import { getPlacesData, getWeatherData } from "./api";
+
 const App = () => {
+    const [places, setPlaces] = useState([])
+    const [coordinates, setCoordinates] = useState({})
+    const [weatherData, setWeatherData] = useState({})
+    const [bounds, setBounds] = useState({})
+    const [childClicked, setChildClicked] = useState(null)
+
+    const [isLoading, setIsLoading] = useState(false)
+    const [type, setType] = useState('restaurants')
+    const [rating, setRating] = useState(0)
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(({coords: {latitude, longitude}}) => {
+            setCoordinates({lat: latitude, lng: longitude})
+        })
+    }, [])
+
+    useEffect(() => {
+        const filteredPlaces = places.filter((place) => place.rating > rating)
+
+        setPlaces(filteredPlaces)
+    }, [rating, places])
+
+    useEffect(() => {
+        if (bounds.sw && bounds.ne) {
+            setIsLoading(true)
+            // console.log(coordinates, bounds)
+
+            getWeatherData(coordinates.lat, coordinates.lng)
+            .then((response) => {
+                if (response) {
+                    // console.log(response)
+                    setWeatherData(response)
+                }
+            })
+            .catch((err) => {
+                console.error(err)
+            })
+
+            getPlacesData(type, bounds)
+            .then((response) => {
+                console.log(response)
+                if(response) {
+                    setPlaces(response.filter((place) => place.name && place.num_reviews > 0))
+                }
+                setIsLoading(false)
+            })
+            .catch((err) => {
+                console.log('failed to get places')
+                setIsLoading(false)
+            })
+        }
+    }, [type, bounds])
+
     return (
         <>
             <CssBaseline />
-            <Header />
+            <Header setCoordinates={setCoordinates}/>
             <Grid container spacing={3} style={{width: '100%'}}>
                 <Grid item xs={12} md={4}>
-                    <List />
+                    <List 
+                        places={places}
+                        childClicked={childClicked}
+                        isLoading={isLoading}
+                        type={type}
+                        setType={setType}
+                        rating={rating}
+                        setRating={setRating}
+                    />
                 </Grid>
                 <Grid item xs={12} md={8}>
-                    <Map />
+                    <Map 
+                        setCoordinates={setCoordinates}
+                        setBounds={setBounds}
+                        coordinates={coordinates}
+                        places={places}
+                        setChildClicked={setChildClicked}
+                        weatherData={weatherData}
+                    />
                 </Grid>
             </Grid>
          </>
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,8 +1,66 @@
-import React from 'react';
+import React, {useState} from 'react';
+import GoogleMapReact from 'google-map-react';
+import {Paper, Typography, useMediaQuery} from '@material-ui/core'
 
-const Map = () => {
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined'
+import Rating from '@material-ui/lab/Rating'
+
+import useStyles from "./MapStyle"
+import mapStyles from './mapStyles';
+
+const Map = ({setCoordinates, setBounds, coordinates, places, setChildClicked, weatherData}) => {
+    const classes = useStyles()
+    const isMobile = useMediaQuery('(max-width: 600px)')
+
     return (
-        <h1>Map</h1>
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY}}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={[50, 50, 50, 50]}
+                options={{disableDefaultUI: true, zoomControl: true, styles: mapStyles}}
+                onChange={(e) => {
+                    // console.log(e)
+                    setCoordinates({lat: e.center.lat, lng: e.center.lng})
+                    setBounds({ne: e.marginBounds.ne, sw: e.marginBounds.sw})
+                }}
+                onChildClick={(child) => setChildClicked(child)}
+            >
+                {places?.map((place, index) => (
+                    <div
+                        className={classes.markerContainer}
+                        lat={Number(place.latitude)}
+                        lng={Number(place.longitude)}
+                        key={index}
+                    >
+                        {
+                            isMobile ? (
+                                <LocationOnOutlinedIcon color="primary" fontSize='large' />
+                            ):(
+                                <Paper elevation={3} className={classes.paper}>
+                                    <Typography className={classes.ypography} variant="subtitle2" gutterBottom>
+                                        {place.name}
+                                    </Typography>
+                                    <img
+                                        className={classes.pointer}
+                                        src={place.photo ? place.photo.images.large.url : "https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg"}
+                                        alt={place.name}
+                                    />
+                                    <Rating size="small" value={Number(place.rating)} readOnly />
+                                </Paper>
+                            )
+                        }
+                    </div>
+                ))}
+                {weatherData?.coord && (
+                    <div lat={weatherData.coord.lat} lng={weatherData.coord.lng}>
+                        <img src={`https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`} alt={'weather icon'} height={100}/>
+                    </div>
+                )}
+            </GoogleMapReact>
+        </div>
     )
 }
 
